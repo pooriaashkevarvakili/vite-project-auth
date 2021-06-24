719c315163ecd3e81befd1e684f1150669f5a735
pooriavakili <pooriavakili09@gmail.com>
2021-06-24 11:33:47 +0430

    firstcommit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="grid place-content-center mt-64">
+<a-input class="bg-green-500 mt-5"/>
+<input class="bg-blue-500 mt-5"/>
+
+<button class="bg-red-500 p-2 py-2 mt-5">pooria</button>
+<a-button class="bg-green-500 mt-5" type="button">login</a-button>
+</div>
+</template>
+
+<script>
+export default {
+name:'home'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/tailwind.css';
+import './assets/style.css';
+import Antd from 'ant-design-vue';
+import "ant-design-vue/dist/antd.css";
+import store from './store';
+import router from "./router";
+const app=createApp(App).use(store)
+app.use(store)
+app.use(router)
+app.use(Antd)
+.mount('#app')
